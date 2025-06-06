946baa4aa6246f9a838637c5a266cee7be978130
Nikit P <nikit@iamdave.ai>
2025-06-07 00:55:06 +0530

    convert and delete

--- convert.py
+++ convert.py
@@ -8,6 +8,7 @@ for i in files:
     fname = i.split('.')[0]
     out = fname + '.mp4'
     os.system('ffmpeg -y -i {} -c copy {}'.format(i, out))
+    os.remove(i)
 
     #os.system('ffmpeg -y -i {} -c:v libx264 -preset veryslow -crf 0 -c:a copy {}'.format(i, out))
 
